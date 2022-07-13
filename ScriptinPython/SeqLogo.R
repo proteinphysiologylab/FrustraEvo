551b5dfc9bd4fe762c9102ff81fc6b31a23b020e
mariafreiberger <37904042+mariafreiberger@users.noreply.github.com>
2022-07-13 11:02:32 +0200

    Delete ScriptinPython directory

--- ScriptinPython/SeqLogo.R
+++
@@ -1,6 +0,0 @@
-library('RWebLogo')
-N=read.delim("long.txt", stringsAsFactors=F, header=F)
-aln=readLines('Logo.fasta')
-# Generate the logo in the file
-weblogo(seqs=aln, format='png',stacks.per.line = N$V1, color.scheme = 'chemistry', file.out='seqlogo.png')
-dev.off()
