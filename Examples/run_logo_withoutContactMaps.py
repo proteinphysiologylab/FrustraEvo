3ed5584402b77fcf64c8b1d1cce6d10ad3b848a4
mariafreiberger <37904042+mariafreiberger@users.noreply.github.com>
2022-08-01 10:37:07 -0300

    Update run_logo_withoutContactMaps.py

--- Examples/run_logo_withoutContactMaps.py
+++ Examples/run_logo_withoutContactMaps.py
@@ -11,6 +11,8 @@ parser.add_argument("--RPath", default='Scripts', help="Path to R script files (
 parser.add_argument("--fasta", help="Name of the fasta")
 parser.add_argument("--ref", default='None', help="Id of the reference protein of your logo (Default: None)")
 parser.add_argument("--pdb_db", default='None', help="Path to the PDBs folder (Default: None)")
+parser.add_argument("--cmaps", default='None', help="Put 'yes' for contactmaps calculation (Default: None)")
+
 
 #How to run the pipeline in linux terminal:
 #python run_logo.py JobId RPath fasta list_file ref pdb_db 
@@ -25,7 +27,7 @@ Functions.copyfiles(args.JobId,args.RPath,args.pdb_db)
 list_file=Functions.pdb_list(args.fasta)
 print('Changes in MSA Frustration Logo')
 Functions.changes(args.JobId,args.fasta)
-print('Running Frutration')
+print('Running Frustration')
 Functions.FrustraPDB(list_file,args.JobId,args.pdb_db)
 print('Running Checks')
 Functions.checks(args.JobId)
@@ -40,5 +42,13 @@ print('Running Checks')
 Functions.LogoCheck(args.JobId)
 print('Making the plots')
 Functions.plots_logo(args.JobId,args.ref,args.RPath)
+if args.cmaps == 'yes':
+	print('Running CMaps for Mutational')
+	Functions.CMaps_Mutational(args.JobId,args.RPath,args.ref)#Genera los mapas de contacto para el indice mutational
+	print('Running CMaps for Configurational')
+	Functions.CMaps_Configurational(args.JobId,args.RPath,args.ref)#Genera los mapas de contacto para el indice configurational
+Functions.clean_files(args.JobId)
 print('Making Visualization scripts (.pml)')
 Functions.VScript(args.JobId)
+os.system('cp Functions.py FrustraEvo_'+args.JobId)
+os.system('cp run_logo.py FrustraEvo_'+args.JobId)
