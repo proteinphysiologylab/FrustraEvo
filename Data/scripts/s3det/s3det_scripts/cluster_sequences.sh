72f1683dbd0f72573e2ab1e758fd1363288a296a
Maria Ines Freiberger <37904042+mariafreiberger@users.noreply.github.com>
2023-04-04 11:42:37 -0300

    Delete Data directory

--- Data/scripts/s3det/s3det_scripts/cluster_sequences.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-for value in orf3a orf3b orf6 orf7a orf7b orf8 orf9b orf9c orf10 E M N S nsp{1..16}
-do
-	cd-hit -i msas/$value.fasta -o cd_hit_clusters/$value.c98 -c 0.98 -s 0.9
-done
--- Data/scripts/s3det/s3det_scripts/run_s3_det.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-for value in N S M E orf3a orf3b orf6 orf7a orf7b orf8 orf9b orf9c orf10 nsp{1..16}
-do
-	./s3det_linux64 -i ../../msas/sub_$value.fasta -o ../../S3_det_results/2nd_round_$value.S3det
-done
