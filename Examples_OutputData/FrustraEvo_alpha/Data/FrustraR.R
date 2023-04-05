763c135dd8ca4601cc538f169caf73d0336640ae
proteinphysiologylab <proteinphysiologylab@gmail.com>
2023-04-05 12:17:26 +0200

    Delete Examples_OutputData/FrustraEvo_alpha directory

--- Examples_OutputData/FrustraEvo_alpha/Data/FrustraR.R
+++
@@ -1,4 +0,0 @@
-library(frustratometeR)
-PdbsDir <- '/home/maria/Documentos/EvoFrustra/FrustraEvo_alpha/Frustration/'
-ResultsDir <-'/home/maria/Documentos/EvoFrustra/FrustraEvo_alpha/Frustration/'
-dir_frustration(PdbsDir = PdbsDir, Mode = 'singleresidue', ResultsDir = ResultsDir)
