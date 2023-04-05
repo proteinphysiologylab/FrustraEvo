7da2c4917a15b5c314850e2d2bf33dd8392a048e
proteinphysiologylab <proteinphysiologylab@gmail.com>
2023-04-05 02:42:47 +0200

    Add files via upload

---
+++ Examples_OutputData/FrustraEvo_alpha/Data/FrustraR.R
@@ -0,0 +1,4 @@
+library(frustratometeR)
+PdbsDir <- '/home/maria/Documentos/EvoFrustra/FrustraEvo_alpha/Frustration/'
+ResultsDir <-'/home/maria/Documentos/EvoFrustra/FrustraEvo_alpha/Frustration/'
+dir_frustration(PdbsDir = PdbsDir, Mode = 'singleresidue', ResultsDir = ResultsDir)
